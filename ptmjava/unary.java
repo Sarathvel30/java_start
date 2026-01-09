aba6cc3e17a5ecae44b239aeaedcb16b3bef8098
Sarathvel30 <157451287+Sarathvel30@users.noreply.github.com>
2026-01-09 10:51:43 +0530

    Add files via upload

---
+++ ptmjava/unary.java
@@ -0,0 +1,11 @@
+public class unary{
+    public static void main(String[] args) {
+        int a =5;
+        int b=10;
+        System.out.println("increment:"+(a++));
+        System.out.println("increment:"+(++a));
+        System.out.println("decrement:"+(b--));
+        System.out.println("decrement:"+(--b));
+        
+    }
+}
